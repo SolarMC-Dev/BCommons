b4a7ebf5d78a69803fe14d0e4ee05374f6af4361
Bradley Steele <26484985+BradleySteele@users.noreply.github.com>
2018-12-11 00:02:26 +0000

    Initial commit

---
+++ src/main/java/me/bradleysteele/commons/register/command/BCommandTabCompleter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Bradley Steele
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.bradleysteele.commons.register.command;
+
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+/**
+ * @author Bradley Steele
+ */
+public interface BCommandTabCompleter {
+
+    /**
+     * @param sender tab sender.
+     * @param alias  alias of the command being tabbed.
+     * @param args   arguments passed.
+     * @return list of tab complete suggestions.
+     */
+    List<String> tabComplete(CommandSender sender, String alias, String[] args);
+
+}
