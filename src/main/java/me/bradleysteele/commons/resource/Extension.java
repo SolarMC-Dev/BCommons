9be73190b48a23d5849788322a3f30edcdd1ac56
Bradley Steele <26484985+BradleySteele@users.noreply.github.com>
2018-12-12 00:00:05 +0000

    Added static method for retrieving an Extension from an alias

--- src/main/java/me/bradleysteele/commons/resource/Extension.java
+++ src/main/java/me/bradleysteele/commons/resource/Extension.java
@@ -19,6 +19,7 @@ package me.bradleysteele.commons.resource;
 import com.google.common.collect.Lists;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * @author Bradley Steele
@@ -30,6 +31,19 @@ public enum Extension {
 
     ;
 
+    /**
+     * Retrieves the {@link Extension} containing the provided
+     * alias.
+     *
+     * @param alias the alias to match.
+     * @return extension with an alias matching the provided alias.
+     */
+    public static Extension from(String alias) {
+        return Stream.of(values())
+                .filter(extension -> extension.getExtensions().contains(alias.toLowerCase()))
+                .findFirst()
+                .orElse(null);
+    }
 
     private final List<String> extensions;
 
