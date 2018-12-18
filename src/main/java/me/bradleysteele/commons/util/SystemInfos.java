82628b874b8688bd0e926918fd5dd62af79f9413
Bradley Steele <26484985+BradleySteele@users.noreply.github.com>
2018-12-18 01:50:02 +0000

    Initial commit

---
+++ src/main/java/me/bradleysteele/commons/util/SystemInfos.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Bradley Steele
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.bradleysteele.commons.util;
+
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
+
+/**
+ * @author Bradley Steele
+ */
+public final class SystemInfos {
+
+    private static final SystemInfo system = new SystemInfo();
+
+    private SystemInfos() {}
+
+    public static SystemInfo getSystemInfo() {
+        return system;
+    }
+
+    public static OperatingSystem getOperatingSystem() {
+        return system.getOperatingSystem();
+    }
+
+    public static HardwareAbstractionLayer getHardware() {
+        return system.getHardware();
+    }
+}
