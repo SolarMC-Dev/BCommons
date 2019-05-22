0af09444b741a4409d49ddaa8e473d350a8b7d51
Bradley Steele <26484985+BradleySteele@users.noreply.github.com>
2019-05-22 19:34:44 +0100

    Added documentation

--- src/main/java/me/bradleysteele/commons/itemstack/ItemStacks.java
+++ src/main/java/me/bradleysteele/commons/itemstack/ItemStacks.java
@@ -17,9 +17,10 @@
 package me.bradleysteele.commons.itemstack;
 
 import me.bradleysteele.commons.itemstack.nbt.NBTItemStack;
+import me.bradleysteele.commons.nms.NMSReflection;
 import me.bradleysteele.commons.resource.ResourceSection;
-import me.bradleysteele.commons.util.reflect.NBTReflection;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectInputStream;
 import org.bukkit.util.io.BukkitObjectOutputStream;
@@ -36,49 +37,99 @@ public final class ItemStacks {
     public static final Material PLAYER_HEAD;
 
     static {
-        PLAYER_HEAD = NBTReflection.isLegacy() ? Material.matchMaterial("SKULL_ITEM") : Material.matchMaterial("PLAYER_HEAD");
+        PLAYER_HEAD = NMSReflection.isLegacy() ? Material.matchMaterial("SKULL_ITEM") : Material.matchMaterial("PLAYER_HEAD");
     }
 
     private ItemStacks() {}
 
+    /**
+     * Returns an item stack builder with the provided material.
+     * <p>
+     * Returns a {@link SkullBuilder} if a {@link ItemStacks#PLAYER_HEAD}
+     * is provided.
+     *
+     * @param material the stack material type.
+     * @return item stack builder with the provided material type.
+     *
+     * @see ItemStackBuilder
+     */
     public static ItemStackBuilder builder(Material material) {
-        return NBTReflection.isLegacy() && material == PLAYER_HEAD ? new SkullBuilder() : new ItemStackBuilder(material);
+        return material == PLAYER_HEAD ? new SkullBuilder() : new ItemStackBuilder(material);
     }
 
+    /**
+     * Returns a new object of the provided item stack builder,
+     * with the same attributes.
+     *
+     * @param builder the builder to copy.
+     * @return a copy of the provided item stack builder.
+     *
+     * @see ItemStackBuilder
+     */
     public static ItemStackBuilder builder(ItemStackBuilder builder) {
         return new ItemStackBuilder(builder);
     }
 
-    public static ItemStackBuilder builder(ItemStack item) {
-        return new ItemStackBuilder(item);
+    /**
+     * Wraps the provided stack into an item stack builder.
+     *
+     * @param stack the stack to wrap into a builder.
+     * @return wrapped item stack builder.
+     *
+     * @see ItemStackBuilder
+     */
+    public static ItemStackBuilder builder(ItemStack stack) {
+        return stack.getType() == PLAYER_HEAD ? new SkullBuilder(stack) : new ItemStackBuilder(stack);
     }
 
+    /**
+     * @return item stack builder with type {@link Material#AIR}.
+     *
+     * @see ItemStackBuilder
+     */
     public static ItemStackBuilder builder() {
         return builder(Material.AIR);
     }
 
+    /**
+     * @param owner the skull owner (player name).
+     * @return skull builder with the provided owner.
+     *
+     * @see SkullBuilder
+     */
     public static SkullBuilder skullBuilder(String owner) {
         return new SkullBuilder(owner);
     }
 
+    /**
+     * @return blank skull builder.
+     *
+     * @see SkullBuilder
+     */
     public static SkullBuilder skullBuilder() {
         return skullBuilder(null);
     }
 
-    public static NBTItemStack toNBTItemStack(ItemStack item) {
-        return new NBTItemStack(item);
+    /**
+     * Wraps the provided stack into an {@link NBTItemStack}.
+     *
+     * @param stack the stack to convert.
+     * @return wrapped NBT stack.
+     *
+     * @see NBTItemStack
+     */
+    public static NBTItemStack toNBTItemStack(ItemStack stack) {
+        return new NBTItemStack(stack);
     }
 
     /**
-     * Unwraps a {@link ResourceSection} into a {@link ItemStack}.
-     *
      * @param section resource section to unwrap.
-     * @return unwrapped item stack.
+     * @return item stack builder with attributes provided by
+     *         the resource section.
+     *
+     * @see ResourceSection
+     * @see ItemStackBuilder
      */
-    public static ItemStack toItemStack(ResourceSection section) {
-        return toItemStackBuilder(section).build();
-    }
-
     public static ItemStackBuilder toItemStackBuilder(ResourceSection section) {
         if (section == null) {
             return null;
@@ -86,7 +137,8 @@ public final class ItemStacks {
 
         int amount = section.getInt("amount", 1);
 
-        return builder(Material.matchMaterial(section.getString("material", "AIR")))
+        return builder()
+                .withMaterial(Material.matchMaterial(section.getString("material", "AIR")))
                 .withAmount(amount < 1 ? 1 : amount > 64 ? 64 : amount)
                 .withDurability(section.getShort("damage", (short) 0))
                 .withDisplayNameColoured(section.getString("name"))
@@ -94,24 +146,61 @@ public final class ItemStacks {
                 .withUnbreakable(section.getBoolean("unbreakable"));
     }
 
+    /**
+     * Unwraps a {@link ResourceSection} into a {@link ItemStack}.
+     *
+     * @param section resource section to unwrap.
+     * @return unwrapped item stack.
+     *
+     * @see ResourceSection
+     */
+    public static ItemStack toItemStack(ResourceSection section) {
+        return toItemStackBuilder(section).build();
+    }
+
+    /**
+     * @param player the player's name.
+     * @return stack with the provided player name as
+     *         the skull.
+     *
+     * @see SkullBuilder
+     */
     public static ItemStack skullOf(String player) {
         return skullBuilder()
                 .withOwner(player)
                 .build();
     }
 
+    public static ItemStack skullOf(OfflinePlayer player) {
+        return skullOf(player.getName());
+    }
+
+    /**
+     * @param url the skull's url.
+     * @return stack with the provided url as the skull.
+     *
+     * @see SkullBuilder
+     */
     public static ItemStack skullOfURL(String url) {
         return skullBuilder()
                 .withURL(url)
                 .build();
     }
 
+    /**
+     * @param stack the stack to test.
+     * @return {@code true} if the stack is null or {@link Material#AIR}.
+     */
     public static boolean isBlank(ItemStack stack) {
         return stack == null || stack.getType() == Material.AIR;
     }
 
+    /**
+     * @param stack the stack to test.
+     * @return {@code true} if the stack is a player head.
+     */
     public static boolean isPlayerHead(ItemStack stack) {
-        return stack.getType() == PLAYER_HEAD && (!NBTReflection.isLegacy() || stack.getDurability() == 3);
+        return stack.getType() == PLAYER_HEAD && (!NMSReflection.isLegacy() || stack.getDurability() == 3);
     }
 
     public static String serializeItems(ItemStack[] items) {
